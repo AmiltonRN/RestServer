dd4aa549aeae6852aa8d6cbb6407480ad6f01d19
amiltonrn <amilton.r.n@hotmail.com>
2016-10-27 23:40:01 -0300

    SQLAlChemy

--- server.py
+++ server.py
@@ -2,116 +2,62 @@ import json, requests, xml.etree.ElementTree as etree
 import mysql.connector as mysql
 import json
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy as SQL
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@127.0.0.1/brasil'
+db = SQL(app)
 
 response = requests.get("http://www.camara.leg.br/SitCamaraWS/Deputados.asmx/ObterDeputados")
+columns = ('id', 'nome', 'matricula', 'partido', 'condicao', 'urlFoto')
 
+class Deputado(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(80))
+    matricula = db.Column(db.String(80))
+    partido = db.Column(db.String(80))
+    urlFoto = db.Column(db.String(160))
+    condicao = db.Column(db.String(80))
 
-class Deputado(object):
     def __init__(self, nome, matricula, partido, urlFoto, condicao):
-        self._nome = nome
-        self._matricula = matricula
-        self._partido = partido
-        self._urlFoto = urlFoto
-        self._condicao = condicao
+        self.nome = nome
+        self.matricula = matricula
+        self.partido = partido
+        self.urlFoto = urlFoto
+        self.condicao = condicao
+
+    def __toJson__(self):
+        return dict(zip(columns, (self.id, self.nome, self.matricula, self.partido, self.urlFoto, self.condicao)))
 
 
-def jdefault(object):
-    return object.__dict__
-
-
-def open_connect():
-    print("Conexao estabelecida")
-    return mysql.connect(database="brasil", user="root", password="", host="127.0.0.1")
-
-
-def save(deputados):
-    try:
-        conn = open_connect()
-        if conn.is_connected():
-            c = conn.cursor()
-            for deputado in deputados:
-                query = "INSERT INTO deputados VALUES(0, %s, %s, %s, %s, %s)"
-                args = (deputado._nome, deputado._matricula,
-                        deputado._partido, deputado._urlFoto,
-                        deputado._condicao)
-                c.execute(query, args)
-            conn.commit()
-            conn.close()
-        else:
-            print("Conexao falhou")
-
-    except mysql.Error as e:
-        print(e)
+@app.route('/')
+def all():
+    db.drop_all()
+    db.create_all()
+    return 'Hello Word'
 
 
 @app.route('/deputados/save', methods=['GET'])
 def api_deputadosSave():
-    deputados = []
     if response.status_code == 200:
-        for child in etree.fromstring(response.content).getiterator("deputado"):
-            deputados.append(Deputado(child.find('nome').text, child.find('matricula').text,
-                                      child.find('partido').text, child.find('urlFoto').text,
-                                      child.find('condicao').text))
-        save(deputados)
+        try:
+            for child in etree.fromstring(response.content).getiterator("deputado"):
+                db.session.add(Deputado(child.find('nome').text, child.find('matricula').text,
+                                          child.find('partido').text, child.find('urlFoto').text,
+                                          child.find('condicao').text))
+            db.session.commit()
+        except mysql.Error as e:
+            print(e)
+
     return "Found"
-columns = ('id', 'nome', 'matricula', 'partido', 'condicao','urlFoto');
 
 
 @app.route('/deputados/get', methods=['GET'])
 def api_deputadosGet():
-    results = [];
-    conn = open_connect()
-    c = conn.cursor()
-    c.execute("SELECT id, nome, matricula, partido, condicao, urlFoto FROM deputados")
-    for row in c.fetchall():
-        results.append(dict(zip(columns, row)))
-    retorno = json.dumps(results)
-    c.close()
-    conn.close()
-    return retorno
-    # table = "<center><table border=1><tr><th>id</th><th>Nome</th><th>Partido</th><th>Foto</th><th>Condicao</th></tr>"
-    # for row in c.fetchall():
-    #     table = table + '<tr><td>' + str(row[0]) + '</td><td>'
-    #     table = table + row[1] + "</td><td>"
-    #     table = table + row[2] + "</td><td><a href = "
-    #     table = table + row[3] + "> Veja a Foto do Elemento(A) </a></td><td>"
-    #     table = table + row[4] + "</tr></td>"
-    # return table + "</table></center>"
-
-
-# @app.route('/deputadosJson', methods=['GET'])
-# def api_deputadosJson():
-#     deputados = []
-#     if response.status_code == 200:
-#         for child in etree.fromstring(response.content).getiterator("deputado"):
-#             deputados.append(Deputado(child.find('nome').text,
-#                                       child.find('matricula').text,
-#                                       child.find('partido').text,
-#                                       child.find('urlFoto').text,
-#                                       child.find('condicao').text))
-#         return (json.dumps(deputados, default=jdefault))
-#     else:
-#         return "not found"
-#
-#
-# @app.route('/deputadosXml', methods=['GET'])
-# def api_deputadosXml():
-#     # response = requests.get("http://www.camara.leg.br/SitCamaraWS/Deputados.asmx/ObterDeputados")
-#     deputados = []
-#     if response.status_code == 200:
-#         table = "<center><table border=1><tr><th>Nome</th><th>Partido</th><th>Foto</th><th>Condicao</th></tr>"
-#         for child in etree.fromstring(response.content).getiterator("deputado"):
-#             table = table + "<tr><td>" + child.find('nome').text + "</td><td>"
-#             table = table + child.find('partido').text + "</td><td><a href = "
-#             table = table + child.find('urlFoto').text + "> Veja a Foto do Elemento(A) </a></td><td>"
-#             table = table + child.find('condicao').text + "</tr></td>"
-#         return table + "</table></center>"
-#     else:
-#         return "not found"
-
+    results = []
+    for row in db.session.query(Deputado).all():
+        results.append(row.__toJson__())
+    return json.dumps(results)
 
 if __name__ == '__main__':
-    app.run(port=5003)
-    # connect()
+    app.run(port=5003, debug=True, threaded=True, host='0.0.0.0')
